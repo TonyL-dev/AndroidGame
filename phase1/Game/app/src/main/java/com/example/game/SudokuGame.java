1b65d50875c3a66e5ccb9035410d633ce563201d
Jianan Tao <ttony.tao@mail.utoronto.ca>
2019-10-29 18:02:26 -0400

    Try again

--- phase1/Game/app/src/main/java/com/example/game/SudokuGame.java
+++ phase1/Game/app/src/main/java/com/example/game/SudokuGame.java
@@ -45,7 +45,7 @@ public class SudokuGame {
 //        //x is the column of the input,y is the row of the input.
 //        //!!!gameboard is a changing variable derived from the modifying funciotn.
 //
-////    }
+//    }
 //
 //    public boolean check_3x3_availability(int a, int x, int y, int[][] gameboard){
 //        //To check whether a user input is possible.
